51e02e69800bc0e743b7c5fd482f33ebbb2b0735
João Carlos Nogueira <76751329+JoaoCarlosNogueira@users.noreply.github.com>
2023-01-16 16:45:10 -0400

    new way of to implement roles

--- src/main/java/com/joaocarlos/ParkingControll/security/WebSecurityConfig.java
+++ src/main/java/com/joaocarlos/ParkingControll/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -23,9 +25,11 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .authorizeHttpRequests()
+                /*
                 .antMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
                 .antMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
+                 */
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
